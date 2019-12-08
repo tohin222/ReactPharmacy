c23f81f9d49555f7a9cb4afebb41456edc786239
tohin222 <vampirehaunted24@gmail.com>
2019-12-09 01:32:25 +0600

    Updates

---
+++ precache-manifest.5a172610361513fdb1905b7fd40a2e66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d6a4b1e54eba63d0ff8b5e309a3cb6",
+    "url": "/ReactPharmacy/index.html"
+  },
+  {
+    "revision": "702176e7b166124495c5",
+    "url": "/ReactPharmacy/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0c3188202b072c6aedf1",
+    "url": "/ReactPharmacy/static/js/2.e4249e16.chunk.js"
+  },
+  {
+    "revision": "702176e7b166124495c5",
+    "url": "/ReactPharmacy/static/js/main.e8698ac4.chunk.js"
+  },
+  {
+    "revision": "ba99dd0718ba410aef12",
+    "url": "/ReactPharmacy/static/js/runtime-main.6acc56a6.js"
+  }
+]);
